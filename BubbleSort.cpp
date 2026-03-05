a2a30d0dba0ca7d5682de9bc1a9733ca647fc14b
febianvalentino543-ui <febianvalentino543@gmail.com>
2026-03-05 10:37:51 +0700

    menambahkan deklarasi dan prosedur awal

--- BubbleSort.cpp
+++ BubbleSort.cpp
@@ -2,5 +2,17 @@
 #include <string>
 using namespace std;
 
+int arr[20];
+int n;
 
+void input() {
+    while (true) {
+        cout << "Masukkan banyaknya elemen pada array: ";
+        cin >> n;
+
+        if (n <= 20)
+            break;
+        else
+            cout << "\nArray dapat memiliki maksimal 20 elemen.\n";
+    }
 }
