6d9460189d52b48c86b295e34be41d47e6b9c314
febianvalentino543-ui <febianvalentino543@gmail.com>
2026-03-05 10:43:40 +0700

    menambahkan penjelasan keterangan

--- BubbleSort.cpp
+++ BubbleSort.cpp
@@ -2,17 +2,17 @@
 #include <string>
 using namespace std;
 
-int arr[20];
-int n;
+int arr[20]; // dekalarasi variabel global array a dengan ukuran 20
+int n; // deklarasi variabel global n untuk menyimpan banyaknya elemen pada array
 
-void input() {
+void input() {          // prosedur untuk input
     while (true) {
-        cout << "Masukkan banyaknya elemen pada array: ";
-        cin >> n;
-
-        if (n <= 20)
-            break;
-        else
-            cout << "\nArray dapat memiliki maksimal 20 elemen.\n";
+        cout << "Masukkan banyaknya elemen pada array: ";       // output ke layar
+        cin >> n;       // input dari pengguna
+        if (n <= 20)    // jika n kurang dari atau sama dengan 20 
+            break;      // keluar dari loop
+        else            // jika n lebih dari 20
+            cout << "\nArray dapat memiliki maksimal 20 elemen.\n";     //tampilkan pesan error
     }
 }
+
